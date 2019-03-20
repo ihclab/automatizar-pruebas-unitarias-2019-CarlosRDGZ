602f4a64e3c6b93821ee73eccb1bcad5297a989e
Carlos Francisco Rodriguez Aguilar <cfr4123@gmail.com>
2019-03-20 00:02:21 -0600

    Problema 2. leer cada caso de prueba

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "Medias.h"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    string line = "";
+    ifstream* file = new ifstream("CasosPrueba.txt");
+
+    if (file->is_open())
+    {
+        while(getline(*file, line))
+        {
+            cout << line << endl;
+        }
+    }
+
+    file->close();
+
     return 0;
 }
