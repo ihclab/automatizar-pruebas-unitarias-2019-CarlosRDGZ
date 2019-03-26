c4ac61e6a4ac54ee579f3dc83526c34812ec9f20
Carlos Francisco Rodriguez Aguilar <cfr4123@gmail.com>
2019-03-26 01:10:35 -0600

    Actividad 2. Determinar el tiempo que llevó ejecutar el caso de prueba.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <regex>
 #include <string>
@@ -12,8 +14,10 @@ using namespace std;
 
 double cast(string input);
 vector<double>* cast(vector<string>* input);
-void printTestResult(string id, bool result, int &var);
+void printTestResult(string id, bool result, double elapsedTime, int &var);
 void printResume(int passed, int failed, int total);
+template<class F>
+bool callTest(F function, string params, string result, double &elapsedTime);
 
 int main()
 {
@@ -28,31 +32,32 @@ int main()
 
     for (int i = 0, iLength = cases->size(); i < iLength; i++)
     {
+        double elapsedTime = 0.0;
         vector<string>* c = cases->at(i);
         try
         {
             if (MethodsMedias::MEDIA_ARITMETICA.compare(c->at(1)) == 0)
-                result = Tester::testEqual(Medias::mediaAritmetica, cast(split(c->at(2), ' ')), cast(c->at(3)));
+                result = callTest(Medias::mediaAritmetica, c->at(2), c->at(3), elapsedTime);
             else if (MethodsMedias::MEDIA_ARMONICA.compare(c->at(1)) == 0)
-                result = Tester::testEqual(Medias::mediaArmonica, cast(split(c->at(2), ' ')), cast(c->at(3)));
+                result = callTest(Medias::mediaArmonica, c->at(2), c->at(3), elapsedTime);
             else if (MethodsMedias::MEDIA_GEOMETRICA.compare(c->at(1)) == 0)
-               result = Tester::testEqual((new Medias())->mediaAritmetica, cast(split(c->at(2), ' ')), cast(c->at(3)));
+                result = callTest((new Medias())->mediaAritmetica, c->at(2), c->at(3), elapsedTime);
             else
             {
                 if (c->at(3).compare("Exception"))
-                    printTestResult(c->at(0), true, passed);
+                    printTestResult(c->at(0), true, elapsedTime, passed);
                 else
-                    printTestResult(c->at(0), false, failed);
+                    printTestResult(c->at(0), false, elapsedTime, failed);
                 continue;
             }
-            printTestResult(c->at(0), result, (result ? passed : failed));
+            printTestResult(c->at(0), result, elapsedTime, (result ? passed : failed));
         }
         catch (const string ex)
         {
             if (ex.compare("Exception") == 0)
-                printTestResult(c->at(0), true, passed);
+                printTestResult(c->at(0), true, elapsedTime, passed);
             else
-                printTestResult(c->at(0), false, failed);
+                printTestResult(c->at(0), false, elapsedTime, failed);
         }
     }
     printResume(passed, failed, cases->size());
@@ -90,7 +95,7 @@ vector<double>* cast(vector<string>* input)
     return values;
 }
 
-void printTestResult(string id, bool result, int &var)
+void printTestResult(string id, bool result, double elapsedTime, int &var)
 {
     HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
     if (result)
@@ -98,7 +103,9 @@ void printTestResult(string id, bool result, int &var)
     else
         SetConsoleTextAttribute(color, 12); // Red
     
-    cout << id << ": " << (result ? "Success" : "Fail") << endl;
+    cout << id << ": " << (result ? "Success. " : "Fail.    ");
+    SetConsoleTextAttribute(color, 7); // White
+    cout << "Elapsed Time: " << fixed << elapsedTime << setprecision(5) << " milliseconds." << endl;
     var++;
 }
 
@@ -107,4 +114,19 @@ void printResume(int passed, int failed, int total)
     HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(color, 7);
     cout << "\nTotal Test: " << total << ". " << "Passed: " << passed << ". " << "Failed: " << failed << "." << endl;
+}
+
+template<class F>
+bool callTest(F function, string params, string returned, double &elapsedTime)
+{
+    chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+
+    bool result = Tester::testEqual(function, cast(split(params, ' ')), cast(returned));
+    Sleep(100);
+    
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    
+    elapsedTime = chrono::duration<double,milli>(end - start).count() - 100;
+
+    return result;
 }
